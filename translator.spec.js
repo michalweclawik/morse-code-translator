a86193800b399d6dbac3a0255b4312eacb644cf9
michal <michalwagh@gmail.com>
2022-09-01 19:30:24 +0100

    tests example

--- translator.spec.js
+++ translator.spec.js
@@ -14,3 +14,39 @@ describe("Testing translateToMorse, turn 'a' in to morse code", () => {
     })
 
 })
+describe("Testing translateToMorse, turn 'Sunny Sunday' in to morse code to test capital letters and space", () => {
+    it("a should translate to be *-", () => {
+        // Arrange
+        let string;
+        // Act
+        string = "Sunny Sunday'";
+        let result = translateToMorse(string);
+        // Assert
+        expect(result).toBe("... ..- -. -. -.-- / ... ..- -. -.. .- -.--")
+    })
+
+})
+describe("Testing translateToMorse, turn '1 2 3 4 5 ' in to morse code to test numbers and space", () => {
+    it("a should translate to be *-", () => {
+        // Arrange
+        let string;
+        // Act
+        string = "1 2 3 4 5 ";
+        let result = translateToMorse(string);
+        // Assert
+        expect(result).toBe(".---- / ..--- / ...-- / ....- / .....")
+    })
+
+})
+describe("Testing translateToMorse, turn 'How are you?' in to morse code to test letters,  space, question mark ", () => {
+    it("a should translate to be *-", () => {
+        // Arrange
+        let string;
+        // Act
+        string = "How are you?";
+        let result = translateToMorse(string);
+        // Assert
+        expect(result).toBe(".... --- .-- / .- .-. . / -.-- --- ..- ..--..")
+    })
+
+})
